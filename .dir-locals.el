92191512a29f1870546f23a7e86d2c02cccf9cf9
jiacai2050 <dev@liujiacai.net>
2022-06-25 22:53:58 +0800

    Update dynamic module post

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((org-mode . ((eval . (add-hook 'before-save-hook 'time-stamp nil t)))))
+((org-mode . ((org-babel-default-header-args:bash . ((:dir . "/tmp")))
+              (eval . (add-hook 'before-save-hook 'time-stamp nil t)))))
