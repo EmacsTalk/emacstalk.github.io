591bd22fd06dc453b10fca86afa6488a7cb2f229
jiacai2050 <dev@liujiacai.net>
2022-09-03 22:42:28 +0800

    Update time-stamp

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,7 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((org-mode . ((org-babel-default-header-args:bash . ((:dir . "/tmp")))
+              (time-stamp-start . "#\\+LASTMOD:[ \t]*")
+              (time-stamp-end . "$")
+              (time-stamp-format . "%Y-%m-%dT%02H:%02M:%02S%5z")
               (eval . (add-hook 'before-save-hook 'time-stamp nil t)))))
